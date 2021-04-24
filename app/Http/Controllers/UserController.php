3c8e5c7f2b54f38cb0980c6e35c2f2fda153f470
geekbim <manyuabim9@gmail.com>
2021-04-24 09:30:08 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class UserController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 300);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+
+        return response()->json(compact('token'));
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name'              => 'required|string|max:255',
+                'email'             => 'required|string|email|max:255|unique:users',
+                'phone'             => 'required|string|min:10',
+                'jenis_kelamin'     => 'required|string|max:255',
+                'tanggal_lahir'     => 'required|date',
+                'password'          => 'required|string|min:6|confirmed'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::create([
+            'name'              => $request->get('name'),
+            'email'             => $request->get('email'),
+            'phone'             => $request->get('phone'),
+            'jenis_kelamin'     => $request->get('jenis_kelamin'),
+            'tanggal_lahir'     => $request->get('tanggal_lahir'),
+            'password'          => Hash::make($request->get('password'))
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user', 'token'), 201);
+    }
+
+    public function logout()
+    {
+        JWTAuth::invalidate(JWTAuth::getToken());
+
+        return response()->json([
+            'message' => 'User successfully sign out'
+        ]);
+    }
+
+    public function refresh()
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+        $token = JWTAuth::refresh($user);
+        
+        return $this->createNewToken($token);
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['User not found'], 404);
+            }
+        } catch (TokenExpiredException $e) {
+            return response()->json(['message' => 'Token iexpired'], 400);
+        } catch (TokenInvalidException $e) {
+            return response()->json(['message' => 'Token Invalid'], 400);
+        } catch (JWTException $e) {
+            return response()->json(['message' => 'Token absent'], 400);
+        }
+
+        return response()->json(compact('user'));
+    }
+
+    protected function createNewToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer', 
+            'expires_in' => JWTAuth::factory()->getTTL() * 60,
+            'user' => Auth::user()
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// auth
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [UserController::class, 'authenticate']);
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::post('/refresh', [UserController::class, 'refresh']);
+    Route::get('/user-profile', [UserController::class, 'getAuthenticatedUser']);
+});
